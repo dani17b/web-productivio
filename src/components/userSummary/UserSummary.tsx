7d842e15347deabda2c3f9061e005433f1b96646
Paula Seoane <paula.seoane@altia.es>
2023-03-14 10:34:14 +0100

    #12 UserSummary's props added

--- src/components/userSummary/UserSummary.tsx
+++ src/components/userSummary/UserSummary.tsx
@@ -1,27 +1,55 @@
+import { useState, useEffect } from 'react';
+import { ContactsNumberProps, CurrentTasksProps, LevelProps } from './actions';
 import './userSummary.scss';
 
 export const UserSummary = () => {
-  // - Imagen de perfil
-  // - Nombre de usuario
-  // - Num misiones en curso
-  // - Nivel
-  // - Total contactos
-  // - Total puntos
+  const [level, setLevel] = useState<LevelProps>({
+    //TODO Añadir lógica real
+    username: 'Logica pendiente',
+    totalPoints: 2,
+    level: 1,
+  });
+
+  const updateLevel = (points: number) => {
+    const pointsPerLevel = 350;
+    const currentLevel = Math.floor(points / pointsPerLevel) + 1;
+    setLevel((previousLevel) => ({ ...previousLevel, level: currentLevel }));
+  };
+
+  useEffect(() => {
+    updateLevel(level.totalPoints);
+  }, [level.totalPoints]);
+
+  const [contacts, setContacts] = useState<ContactsNumberProps>({
+    //TODO Añadir lógica real
+    contactsNumber: 4,
+  });
+
+  const [tasks, setTasks] = useState<CurrentTasksProps>({
+    //TODO Añadir lógica real
+    currentTasks: 2,
+  });
+
   return (
     <div className="user-summary">
       <div className="user-summary__left-column">
         <div className="user-summary__user-pic"></div>
       </div>
       <div className="user-summary__center-column">
-        <div className="user-summary__username">danituriño22</div>
-        <div className="user-summary__current-missions">
-          3 misiones en curso
+        <div className="user-summary__username">
+          danituriño22
+          {/* {userInfo.login} */}
         </div>
-        <div className="user-summary__user-level">Nivel 3</div>
+        <div className="user-summary__current-missions">
+          {tasks.currentTasks} misiones en curso
+        </div>
+        <div className="user-summary__user-level">Nivel {level.level}</div>
       </div>
       <div className="user-summary__right-column">
-        <div className="user-summary__contacts">123 contactos</div>
-        <div className="user-summary__total-points">520pts</div>
+        <div className="user-summary__contacts">
+          {contacts.contactsNumber} contactos
+        </div>
+        <div className="user-summary__total-points">{level.totalPoints}pts</div>
       </div>
     </div>
   );
---
+++ src/components/userSummary/actions.tsx
@@ -0,0 +1,13 @@
+export interface LevelProps {
+  username: string;
+  totalPoints: number;
+  level: number;
+}
+
+export interface ContactsNumberProps {
+  contactsNumber: number;
+}
+
+export interface CurrentTasksProps {
+  currentTasks: number;
+}
