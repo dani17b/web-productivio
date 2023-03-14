88fc1d7be0194567b557ea899be819d96b4f048a
hector.arias <ariashector19@gmail.com>
2023-03-14 13:07:59 +0100

    Structure for UserDetails and scss style

--- src/components/userDetails/UserDetails.tsx
+++ src/components/userDetails/UserDetails.tsx
@@ -1,7 +1,12 @@
 import './userDetails.scss';
 
 export interface UserDetailsProps {
-  user: { username: string; description: string; score: number; img: string };
+  user: {
+    username: string;
+    description: string;
+    score: number;
+    img: string;
+  };
 }
 
 export const UserDetails = (props: UserDetailsProps) => {
@@ -9,12 +14,23 @@ export const UserDetails = (props: UserDetailsProps) => {
 
   return (
     <div className="user-details">
-      <img className="user-details__pic" src={user.img} alt="UserImage" />
-      <div className="user-details__username">
-        <p>{user.username}</p>
+      {/* TODO implementar el componente de la foto de prefil */}
+      <div className="user-details__pic">
+        <img src="../../../logo.svg" alt="UserImage" />
       </div>
-      <p>{user.description}</p>
-      <img className="__user-details-stars" src="" />
+      <div className="user-details__info">
+        <div className="user-details__info__top">
+          <div className="user-details__info__top__username-star">
+            <p>{user.username}</p>
+            <p>estrellas</p>
+          </div>
+          <div className="user-details__info__bottom">
+            <p>{user.description}</p>
+          </div>
+        </div>
+
+        <img className="__user-details-stars" src="" />
+      </div>
     </div>
   );
 };
