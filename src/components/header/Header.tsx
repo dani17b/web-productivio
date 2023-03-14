959dc852e3b6854ac2eb26b511fa86b4d159a22a
Ivan García García <ivan.garcia@altia.es>
2023-03-14 09:40:43 +0100

    Changed from fortawesome library to react-icons

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,15 +1,14 @@
 import './header.scss';
 
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBell } from '@fortawesome/free-solid-svg-icons';
+import { GrNotification } from 'react-icons/gr';
 
 export const Header = () => {
   return (
     <header className="header">
       <h1>Productivio</h1>
       <div className="notifications">
-        <FontAwesomeIcon icon={faBell} />
+        <GrNotification />
         <span className="count">3</span>
       </div>
     </header>
