c5863143ec82874aba42768fac4e1c35a666e58e
Bruno López <bruno.lpgz@gmail.com>
2023-03-21 17:39:07 +0100

    using lib (wip)

--- src/modules/circle/Circle.tsx
+++ src/modules/circle/Circle.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Header } from 'src/components/header/Header';
-import { TeamDropdown } from 'src/components/teamDropdown/TeamDropdown';
+import { TeamDropdown } from 'lib-productivio';
 
 export const Circle = () => {
   return (
@@ -10,29 +10,89 @@ export const Circle = () => {
       <TeamDropdown
         title="EQUIPO A"
         data={[
-          { id: 1, name: 'John' },
-          { id: 2, name: 'Jane' },
-          { id: 3, name: 'Bob' },
+          {
+            username: 'JohnDoe',
+            description: 'Software Engineer',
+            score: 4.5,
+            profileColor: '#FF5733',
+            img: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bWFsZSUyMHByb2ZpbGV8ZW58MHx8MHx8&w=1000&q=80',
+          },
+          {
+            username: 'JaneSmith',
+            description: 'Graphic Designer',
+            score: 3.8,
+            profileColor: '#F5B041',
+            img: 'https://newprofilepic2.photo-cdn.net//assets/images/article/profile.jpg',
+          },
+          {
+            username: 'BobJohnson',
+            description: 'Product Manager',
+            score: 4.2,
+            profileColor: '#48C9B0',
+            img: 'https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+          },
         ]}
         bgColor="#86C4D1"
+        isExpanded={true}
+        onClick={() => {}}
       ></TeamDropdown>
       <TeamDropdown
-        title="FP DUAL"
+        title="EQUIPO A"
         data={[
-          { id: 1, name: 'John' },
-          { id: 2, name: 'Jane' },
-          { id: 3, name: 'Bob' },
+          {
+            username: 'JohnDoe',
+            description: 'Software Engineer',
+            score: 4.5,
+            profileColor: '#FF5733',
+            img: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bWFsZSUyMHByb2ZpbGV8ZW58MHx8MHx8&w=1000&q=80',
+          },
+          {
+            username: 'JaneSmith',
+            description: 'Graphic Designer',
+            score: 3.8,
+            profileColor: '#F5B041',
+            img: 'https://newprofilepic2.photo-cdn.net//assets/images/article/profile.jpg',
+          },
+          {
+            username: 'BobJohnson',
+            description: 'Product Manager',
+            score: 4.2,
+            profileColor: '#48C9B0',
+            img: 'https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+          },
         ]}
         bgColor="#A786D1"
+        isExpanded={true}
+        onClick={() => {}}
       ></TeamDropdown>
       <TeamDropdown
-        title="OTRO"
+        title="EQUIPO A"
         data={[
-          { id: 1, name: 'John' },
-          { id: 2, name: 'Jane' },
-          { id: 3, name: 'Bob' },
+          {
+            username: 'JohnDoe',
+            description: 'Software Engineer',
+            score: 4.5,
+            profileColor: '#FF5733',
+            img: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bWFsZSUyMHByb2ZpbGV8ZW58MHx8MHx8&w=1000&q=80',
+          },
+          {
+            username: 'JaneSmith',
+            description: 'Graphic Designer',
+            score: 3.8,
+            profileColor: '#F5B041',
+            img: 'https://newprofilepic2.photo-cdn.net//assets/images/article/profile.jpg',
+          },
+          {
+            username: 'BobJohnson',
+            description: 'Product Manager',
+            score: 4.2,
+            profileColor: '#48C9B0',
+            img: 'https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+          },
         ]}
         bgColor="#86D18E"
+        isExpanded={true}
+        onClick={() => {}}
       ></TeamDropdown>
     </>
   );
