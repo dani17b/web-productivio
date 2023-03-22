58e10c3cfee8e1ca723d8514cc764060abbb134d
Ivan Lopez Rodriguez <ivan.lopez.rodriguez@altia.es>
2023-03-22 10:28:39 +0100

    commit no styles

--- src/modules/newTask/NewTask.tsx
+++ src/modules/newTask/NewTask.tsx
@@ -1,19 +1,16 @@
-
-import { useEffect, useState } from 'react';
-import "./newTask.scss";
-import {Header} from "lib-productivio";
+import './newTask.scss';
+import { Header, NavBar } from 'lib-productivio';
 
 export const NewTask = () => {
-   
-    return (
-    <div className='new-task'>
-     <div className='header'>
-        <Header count={1}/>        
-     </div>
-     <div className='form'>        
-     </div>
-     <div className='navbar'>        
-     </div>
+  return (
+    <div className="new-task">
+      <div className="header">
+        <Header count={1} />
+      </div>
+      <div className="form"></div>
+      <div className="navbar">
+        <NavBar />
+      </div>
     </div>
-    );
-  };
+  );
+};
