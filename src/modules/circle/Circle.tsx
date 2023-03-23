3a006bd23e53a13916da04b12f7f8947deb9277a
Bruno López <bruno.lpgz@gmail.com>
2023-03-23 10:55:01 +0100

    New TeamDropDown implemented

--- src/modules/circle/Circle.tsx
+++ src/modules/circle/Circle.tsx
@@ -11,88 +11,100 @@ export const Circle = () => {
         title="EQUIPO A"
         data={[
           {
-            username: 'JohnDoe',
-            description: 'Software Engineer',
-            score: 4.5,
-            profileColor: '#FF5733',
-            img: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bWFsZSUyMHByb2ZpbGV8ZW58MHx8MHx8&w=1000&q=80',
+            user: {
+              username: 'JohnDoe',
+              description: 'Software Engineer',
+              score: 4.5,
+              profileColor: '#FF5733',
+              img: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bWFsZSUyMHByb2ZpbGV8ZW58MHx8MHx8&w=1000&q=80',
+            },
           },
           {
-            username: 'JaneSmith',
-            description: 'Graphic Designer',
-            score: 3.8,
-            profileColor: '#F5B041',
-            img: 'https://newprofilepic2.photo-cdn.net//assets/images/article/profile.jpg',
+            user: {
+              username: 'JaneSmith',
+              description: 'Graphic Designer',
+              score: 3.8,
+              profileColor: '#F5B041',
+              img: 'https://newprofilepic2.photo-cdn.net//assets/images/article/profile.jpg',
+            },
           },
           {
-            username: 'BobJohnson',
-            description: 'Product Manager',
-            score: 4.2,
-            profileColor: '#48C9B0',
-            img: 'https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+            user: {
+              username: 'BobJohnson',
+              description: 'Product Manager',
+              score: 4.2,
+              profileColor: '#48C9B0',
+              img: 'https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+            },
           },
         ]}
         bgColor="#86C4D1"
-        isExpanded={true}
-        onClick={() => {}}
       ></TeamDropdown>
       <TeamDropdown
         title="EQUIPO A"
         data={[
           {
-            username: 'JohnDoe',
-            description: 'Software Engineer',
-            score: 4.5,
-            profileColor: '#FF5733',
-            img: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bWFsZSUyMHByb2ZpbGV8ZW58MHx8MHx8&w=1000&q=80',
+            user: {
+              username: 'JohnDoe',
+              description: 'Software Engineer',
+              score: 4.5,
+              profileColor: '#FF5733',
+              img: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bWFsZSUyMHByb2ZpbGV8ZW58MHx8MHx8&w=1000&q=80',
+            },
           },
           {
-            username: 'JaneSmith',
-            description: 'Graphic Designer',
-            score: 3.8,
-            profileColor: '#F5B041',
-            img: 'https://newprofilepic2.photo-cdn.net//assets/images/article/profile.jpg',
+            user: {
+              username: 'JaneSmith',
+              description: 'Graphic Designer',
+              score: 3.8,
+              profileColor: '#F5B041',
+              img: 'https://newprofilepic2.photo-cdn.net//assets/images/article/profile.jpg',
+            },
           },
           {
-            username: 'BobJohnson',
-            description: 'Product Manager',
-            score: 4.2,
-            profileColor: '#48C9B0',
-            img: 'https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+            user: {
+              username: 'BobJohnson',
+              description: 'Product Manager',
+              score: 4.2,
+              profileColor: '#48C9B0',
+              img: 'https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+            },
           },
         ]}
         bgColor="#A786D1"
-        isExpanded={true}
-        onClick={() => {}}
       ></TeamDropdown>
       <TeamDropdown
         title="EQUIPO A"
         data={[
           {
-            username: 'JohnDoe',
-            description: 'Software Engineer',
-            score: 4.5,
-            profileColor: '#FF5733',
-            img: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bWFsZSUyMHByb2ZpbGV8ZW58MHx8MHx8&w=1000&q=80',
+            user: {
+              username: 'JohnDoe',
+              description: 'Software Engineer',
+              score: 4.5,
+              profileColor: '#FF5733',
+              img: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bWFsZSUyMHByb2ZpbGV8ZW58MHx8MHx8&w=1000&q=80',
+            },
           },
           {
-            username: 'JaneSmith',
-            description: 'Graphic Designer',
-            score: 3.8,
-            profileColor: '#F5B041',
-            img: 'https://newprofilepic2.photo-cdn.net//assets/images/article/profile.jpg',
+            user: {
+              username: 'JaneSmith',
+              description: 'Graphic Designer',
+              score: 3.8,
+              profileColor: '#F5B041',
+              img: 'https://newprofilepic2.photo-cdn.net//assets/images/article/profile.jpg',
+            },
           },
           {
-            username: 'BobJohnson',
-            description: 'Product Manager',
-            score: 4.2,
-            profileColor: '#48C9B0',
-            img: 'https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+            user: {
+              username: 'BobJohnson',
+              description: 'Product Manager',
+              score: 4.2,
+              profileColor: '#48C9B0',
+              img: 'https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+            },
           },
         ]}
         bgColor="#86D18E"
-        isExpanded={true}
-        onClick={() => {}}
       ></TeamDropdown>
     </>
   );
