c5853a6646082b5e08e03140aef402d1c07f7bbd
Bruno López <bruno.lpgz@gmail.com>
2023-03-28 17:37:17 +0200

    Test object added, component folder deleted

--- src/modules/circle/Circle.tsx
+++ src/modules/circle/Circle.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Header } from 'src/components/header/Header';
 import { NavBar, SearchBar, TeamDropdown } from 'lib-productivio';
 import './circle.scss';
+import { test } from './TeamDataExample';
 
 //TODO: add search functionality
 const onSearch = (searchTerm: string) => {};
@@ -11,105 +12,7 @@ export const Circle = () => {
     <>
       <Header></Header>
       <SearchBar onSearch={onSearch}></SearchBar>
-      <TeamDropdown
-        title="EQUIPO A"
-        data={[
-          {
-            user: {
-              username: 'JohnDoe',
-              description: 'Software Engineer',
-              score: 4.5,
-              profileColor: '#FF5733',
-              img: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bWFsZSUyMHByb2ZpbGV8ZW58MHx8MHx8&w=1000&q=80',
-            },
-          },
-          {
-            user: {
-              username: 'JaneSmith',
-              description: 'Graphic Designer',
-              score: 3.8,
-              profileColor: '#F5B041',
-              img: 'https://newprofilepic2.photo-cdn.net//assets/images/article/profile.jpg',
-            },
-          },
-          {
-            user: {
-              username: 'BobJohnson',
-              description: 'Product Manager',
-              score: 4.2,
-              profileColor: '#48C9B0',
-              img: 'https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-            },
-          },
-        ]}
-        bgColor="#86C4D1"
-      ></TeamDropdown>
-      <TeamDropdown
-        title="EQUIPO A"
-        data={[
-          {
-            user: {
-              username: 'JohnDoe',
-              description: 'Software Engineer',
-              score: 4.5,
-              profileColor: '#FF5733',
-              img: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bWFsZSUyMHByb2ZpbGV8ZW58MHx8MHx8&w=1000&q=80',
-            },
-          },
-          {
-            user: {
-              username: 'JaneSmith',
-              description: 'Graphic Designer',
-              score: 3.8,
-              profileColor: '#F5B041',
-              img: 'https://newprofilepic2.photo-cdn.net//assets/images/article/profile.jpg',
-            },
-          },
-          {
-            user: {
-              username: 'BobJohnson',
-              description: 'Product Manager',
-              score: 4.2,
-              profileColor: '#48C9B0',
-              img: 'https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-            },
-          },
-        ]}
-        bgColor="#A786D1"
-      ></TeamDropdown>
-      <TeamDropdown
-        title="EQUIPO A"
-        data={[
-          {
-            user: {
-              username: 'JohnDoe',
-              description: 'Software Engineer',
-              score: 4.5,
-              profileColor: '#FF5733',
-              img: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bWFsZSUyMHByb2ZpbGV8ZW58MHx8MHx8&w=1000&q=80',
-            },
-          },
-          {
-            user: {
-              username: 'JaneSmith',
-              description: 'Graphic Designer',
-              score: 3.8,
-              profileColor: '#F5B041',
-              img: 'https://newprofilepic2.photo-cdn.net//assets/images/article/profile.jpg',
-            },
-          },
-          {
-            user: {
-              username: 'BobJohnson',
-              description: 'Product Manager',
-              score: 4.2,
-              profileColor: '#48C9B0',
-              img: 'https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-            },
-          },
-        ]}
-        bgColor="#86D18E"
-      ></TeamDropdown>
+      <TeamDropdown {...test}></TeamDropdown>
       <NavBar></NavBar>
     </>
   );
