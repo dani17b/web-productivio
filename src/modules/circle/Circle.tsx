7488000fb67e7b1a279012d3a5410eb75a799e1c
Bruno López <bruno.lpgz@gmail.com>
2023-03-28 17:44:48 +0200

    Serach working

--- src/modules/circle/Circle.tsx
+++ src/modules/circle/Circle.tsx
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Header } from 'src/components/header/Header';
 import { NavBar, SearchBar, TeamDropdown } from 'lib-productivio';
 import './circle.scss';
 import { test } from './TeamDataExample';
 
 //TODO: add search functionality
-const onSearch = (searchTerm: string) => {};
 
 export const Circle = () => {
+  const onSearch = (searchTerm: string) => {
+    setData((prevData) => ({
+      ...prevData,
+      data: test.data.filter((item) => item.username.includes(searchTerm)),
+    }));
+  };
+
+  const [data, setData] = useState(test);
   return (
     <>
       <Header></Header>
       <SearchBar onSearch={onSearch}></SearchBar>
-      <TeamDropdown {...test}></TeamDropdown>
+      <TeamDropdown {...data}></TeamDropdown>
       <NavBar></NavBar>
     </>
   );
