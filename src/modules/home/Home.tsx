cb1c1e96afb6dcb79d14df2c9bea2cf22b1f7ed7
Xavier Novoa <xnovoavarela@gmail.com>
2023-03-30 12:48:33 +0200

    styling fixed

--- src/modules/home/Home.tsx
+++ src/modules/home/Home.tsx
@@ -4,6 +4,7 @@ import { Header, NavBar } from 'lib-productivio';
 import { getPosts } from './actions';
 import { useDispatch } from 'react-redux';
 import { FeedBlock } from 'src/components/feedBlock/FeedBlock';
+import './home.scss';
 
 export const Home = () => {
   const dispatch = useDispatch();
@@ -17,25 +18,32 @@ export const Home = () => {
     <>
       {!loading && (
         <div>
-          <Header count={0} title='Productivio'/>
-          {posts.map((post: any) => (
-            <FeedBlock
-              borderColor={post.creatorUser.userColor}
-              description={post.description}
-              imageSrc={post.creatorUser.userPicUrl}
-              username={post.creatorUser.name}
-              totalLikes={post.likes}
-              key={post.id}
-              taskProgessBarPercent={post.taskBarProgress}
-              comments={[]}
-              createdAt={post.creationDate}
-            />
-          ))}
-          <NavBar />
+          <div className="header">
+            <Header count={0} title="Productivio" />
+          </div>
+          <div className="home-content">
+            {posts.map((post: any) => (
+              <FeedBlock
+                borderColor={post.creatorUser.userColor}
+                description={post.description}
+                imageSrc={post.creatorUser.userPicUrl}
+                username={post.creatorUser.name}
+                totalLikes={post.likes}
+                key={post.id}
+                taskProgessBarPercent={post.taskBarProgress}
+                comments={[]}
+                createdAt={post.creationDate}
+              />
+            ))}
+          </div>
+          <div className="navbar">
+            <NavBar />
+          </div>
         </div>
-        
       )}
-      {loading && <div>{/*TODO: cambiar esto por la animación global*/ }cargando</div>}
+      {loading && (
+        <div>{/*TODO: cambiar esto por la animación global*/}cargando</div>
+      )}
     </>
   );
 };
