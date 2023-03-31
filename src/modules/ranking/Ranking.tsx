0c27f021cb5a92546476f30b8809815f3089d6f0
Paula Alba Piñeiro <paula.alba@altia.es>
2023-03-31 09:17:14 +0200

    map and styles applied

--- src/modules/ranking/Ranking.tsx
+++ src/modules/ranking/Ranking.tsx
@@ -1,7 +1,7 @@
-import { Header, NavBar, TopOne } from 'lib-productivio';
-import { useEffect, useState } from 'react';
+import { Header, NavBar, Podium, TopFiveUser, TopOne } from 'lib-productivio';
+import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { getRanking, postRanking, RankingProps } from './actions';
+import { getRanking } from './actions';
 import './ranking.scss';
 
 export const Ranking = () => {
@@ -12,43 +12,48 @@ export const Ranking = () => {
     dispatch(getRanking());
   }, [dispatch]);
 
-  useEffect(() => {
-    console.log('Ranking state:', ranking);
-  }, [ranking]);
+  let sortedUsers = [];
+  let usersSlice = [];
+  if (ranking && ranking.ranking) {
+    sortedUsers = ranking.ranking.sort(
+      (a: any, b: any) => b.user.userPoints - a.user.userPoints
+    );
+    usersSlice = sortedUsers.slice(4, 10);
+    console.log(sortedUsers);
+  }
 
-  const [data] = useState<RankingProps>({
-    email: '',
-    name: '',
-    description: '',
-    userPoints: 0,
-    activeTasks: 0,
-    friends: 0,
-    userPicUrl: '',
-    userColor: '',
-  });
   return (
     <div>
       <Header count={1} title={'Productivio'} />
-      Ranking page
-      <TopOne userImg="" userColor="" username="" points=""></TopOne>
-      <NavBar />
-      {ranking.loading && <div>Cargando...</div>}
-      {!ranking.loading && ranking !== null && (
-        <div>
-          {ranking.length}
-          {JSON.stringify(ranking)}
-        </div>
+
+      {sortedUsers.length > 0 && (
+        <TopOne
+          userImg={sortedUsers[0].userPicUrl}
+          userColor={sortedUsers[0].userColor}
+          username={sortedUsers[0].name}
+          points={sortedUsers[0].userPoints}
+        />
       )}
-      {
-        <div>
-          <button
-            onClick={() => {
-              dispatch(postRanking(data));
-            }}
-            
-          > dasdasdasd </button>
-        </div>
-      }
+
+      {sortedUsers.length > 3 && (
+        <Podium users={[sortedUsers[1], sortedUsers[2], sortedUsers[3]]} />
+      )}
+
+      {usersSlice.length > 0 &&
+        usersSlice.map((user: any, index: any) => (
+          <TopFiveUser
+            userImg={user.userPicUrl}
+            username={user.name}
+            userColor={user.userColor}
+            points={user.userPoints}
+            position={index + 5}
+            key={user.id} // Asegúrate de añadir una key única para cada elemento en la lista
+          />
+        ))}
+
+      {ranking.loading && <div>Cargando...</div>}
+
+      <NavBar />
     </div>
   );
 };
