173acef6ba6b8b2c2affa84124c166d3863d4334
Bruno López <bruno.lpgz@gmail.com>
2023-03-31 13:18:43 +0200

    New mapper

--- src/modules/circle/TeamDataMapper.ts
+++ src/modules/circle/TeamDataMapper.ts
@@ -1,14 +1,37 @@
 export const responseToTeamDropdownProps = (response: any, bgColor: string) => {
   return {
     title: response.name,
-    data: response.workers.map((worker: any) => {
+    userInfoProps: response.workers.map((worker: any) => {
       return {
-        username: worker.name,
-        description: worker.description,
-        profileColor: worker.userColor,
-        userImg: worker.userPicUrl,
-      };
+        user: {
+          username: worker.name,
+          description: worker.description,
+          userColor: worker.userColor,
+          userImg: worker.userPicUrl,
+        },
+        backgroundColor: brightenHex(bgColor),
+        textColor: 'black',
+      }
     }),
-    bgColor: bgColor,
+     bgColor: bgColor,
   };
 };
+
+
+
+function brightenHex(hexColor: string) {
+  // parse the hex color to RGB values
+  let red = parseInt(hexColor.substring(1, 3), 16);
+  let green = parseInt(hexColor.substring(3, 5), 16);
+  let blue = parseInt(hexColor.substring(5, 7), 16);
+
+  // calculate the new, brighter RGB values
+  let brightenedRed = Math.round(Math.min(255, red * 1.5));
+  let brightenedGreen = Math.round(Math.min(255, green * 1.5));
+  let brightenedBlue = Math.round(Math.min(255, blue * 1.5));
+
+  // convert the new RGB values back to a hex color string
+  let brightenedHex = '#' + brightenedRed.toString(16).padStart(2, '0') + brightenedGreen.toString(16).padStart(2, '0') + brightenedBlue.toString(16).padStart(2, '0');
+
+  return brightenedHex;
+}
