548cb983a3b4879880ddf8fc7952e21f11d99d76
Bruno López <bruno.lpgz@gmail.com>
2023-04-03 16:19:50 +0200

    Review comments fixed

--- src/modules/circle/TeamDataMapper.ts
+++ src/modules/circle/TeamDataMapper.ts
@@ -11,27 +11,44 @@ export const responseToTeamDropdownProps = (response: any, bgColor: string) => {
         },
         backgroundColor: brightenHex(bgColor),
         textColor: 'black',
-      }
+      };
     }),
-     bgColor: bgColor,
+    bgColor: bgColor,
   };
 };
 
-
-
 function brightenHex(hexColor: string) {
+  const MAX_COLOR_VALUE = 255;
+  const BRIGHTNESS_FACTOR = 1.5;
+  const HEX_LENGTH = 2;
+  const COLOR_BYTES = 16;
+
   // parse the hex color to RGB values
-  let red = parseInt(hexColor.substring(1, 3), 16);
-  let green = parseInt(hexColor.substring(3, 5), 16);
-  let blue = parseInt(hexColor.substring(5, 7), 16);
+  /* eslint-disable */
+  let red = parseInt(hexColor.substring(1, 3), COLOR_BYTES);
+  let green = parseInt(hexColor.substring(3, 5), COLOR_BYTES);
+  let blue = parseInt(hexColor.substring(5, 7), COLOR_BYTES);
+  /* eslint-enable */
 
   // calculate the new, brighter RGB values
-  let brightenedRed = Math.round(Math.min(255, red * 1.5));
-  let brightenedGreen = Math.round(Math.min(255, green * 1.5));
-  let brightenedBlue = Math.round(Math.min(255, blue * 1.5));
+  let brightenedRed = Math.round(
+    Math.min(MAX_COLOR_VALUE, red * BRIGHTNESS_FACTOR)
+  );
+  let brightenedGreen = Math.round(
+    Math.min(MAX_COLOR_VALUE, green * BRIGHTNESS_FACTOR)
+  );
+  let brightenedBlue = Math.round(
+    Math.min(MAX_COLOR_VALUE, blue * BRIGHTNESS_FACTOR)
+  );
 
   // convert the new RGB values back to a hex color string
-  let brightenedHex = '#' + brightenedRed.toString(16).padStart(2, '0') + brightenedGreen.toString(16).padStart(2, '0') + brightenedBlue.toString(16).padStart(2, '0');
+  let brightenedHex = `#${brightenedRed
+    .toString(COLOR_BYTES)
+    .padStart(HEX_LENGTH, '0')}${brightenedGreen
+    .toString(COLOR_BYTES)
+    .padStart(HEX_LENGTH, '0')}${brightenedBlue
+    .toString(COLOR_BYTES)
+    .padStart(HEX_LENGTH, '0')}`;
 
   return brightenedHex;
 }
