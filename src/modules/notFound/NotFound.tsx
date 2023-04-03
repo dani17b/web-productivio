78cb7eaacf44cd77e9234236dd9a7dc623662c11
Bruno López <bruno.lpgz@gmail.com>
2023-04-03 13:46:44 +0200

    header changed only on notfound

--- src/modules/notFound/NotFound.tsx
+++ src/modules/notFound/NotFound.tsx
@@ -5,13 +5,13 @@ import './notFound.scss';
 
 export const NotFound = () => {
   return (
-    <>
+    <div className='notFound'>
       <Header></Header>
       <div>
         <h1>404 - Page Not Found</h1>
         <p>Sorry, the page does not exist (by the moment)</p>
       </div>
       <WebNavBar />
-    </>
+    </div>
   );
 };
