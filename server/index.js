9792e16b644a58f86000e56f245e6d803343e337
Paula Alba Piñeiro <paula.alba@altia.es>
2023-04-13 12:57:08 +0200

    method that gets files from mocks folder

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
 app.use(cors());
@@ -7,30 +9,57 @@ app.use(cors());
 const port = 8081;
 
 const getMock = (method, url) => {
-  
   var mockData = null;
 
-  try{
-    mockData = require(`./mocks${url}/${method}.json`);
-  }catch(e){
+  try {
+    mockData = require(require(`./mocks${url}/${method}.${extension}`));
+  } catch (e) {
     console.log(e);
   }
 
   return mockData;
-}
+};
+
+app.get('/modules', (req, res) => {
+  const moduleDir = path.join(__dirname, 'mocks');
+  const modules = [];
+
+  fs.readdirSync(moduleDir).forEach((moduleName) => {
+    const modulePath = path.join(moduleDir, moduleName);
+    const moduleStat = fs.statSync(modulePath);
+
+    if (moduleStat.isDirectory()) {
+      const moduleFiles = fs.readdirSync(modulePath);
+      moduleFiles.forEach((moduleFile) => {
+        const moduleFilePath = path.join(modulePath, moduleFile);
+        const moduleFileStat = fs.statSync(moduleFilePath);
+
+        if (path.extname(moduleFilePath) === '.json') {
+          modules.push({
+            name: `${moduleName}-${path.basename(moduleFilePath, '.json')}`,
+            path: `/${moduleName}/${path.basename(moduleFilePath, '.json')}`,
+            type: 'screen',
+          });
+        }
+      });
+    }
+  });
+
+  res.json(modules);
+});
 
 app.all('*', (req, res) => {
   const mockData = getMock(req.method.toLowerCase(), req.originalUrl);
 
-  if(mockData == null){
+  if (mockData == null) {
     res.status(404);
     res.end();
     return;
   }
 
   res.send(getMock(req.method.toLowerCase(), req.originalUrl));
-})
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
