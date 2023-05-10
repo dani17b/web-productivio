760d2e289be6db853765de1c129fba2b0b4fe9c7
Bruno López <bruno.lpgz@gmail.com>
2023-05-10 18:03:27 +0200

    finished

--- src/modules/editor/Editor.tsx
+++ src/modules/editor/Editor.tsx
@@ -74,13 +74,12 @@ export const MovableItem = ({ children, onClick }) => {
 export const Editor = () => {
   const [selectedElement, setSelectedElement] = useState(null);
   const [inputValue, setInputValue] = useState('');
-  const {modules} = useSelector((state) => state.editor);
-  const [ path, setPath ] = useState(null);
+  const { modules } = useSelector((state) => state.editor);
+  const [path, setPath] = useState(null);
   const dispatch = useDispatch();
   const [files, setFiles] = useState([]);
   const [modulesList, setModulesList] = useState([]);
   const [setComponentCodeList] = useState([]);
-
 
   //Devuelve un Json con array de objetos con información de los módulos
   useEffect(() => {
@@ -96,45 +95,43 @@ export const Editor = () => {
     fetchData();
   }, []);
 
-  
-
- //Se ejecuta al darle al botón de guardar y contiene dos métodos que construyen los Json que necesita el back para guardar un archivo
+  //Se ejecuta al darle al botón de guardar y contiene dos métodos que construyen los Json que necesita el back para guardar un archivo
   const handleSave = () => {
-      const buildTsxJsonToSave = {
-       filename: inputValue + '.tsx',
-       content: parseJsonToTsx(modules[0])   
-   }
-   saveOrUpdate(buildTsxJsonToSave);
-   console.log(buildTsxJsonToSave);
+    const buildTsxJsonToSave = {
+      filename: inputValue + '.tsx',
+      content: parseJsonToTsx(modules[0]),
+    };
+    saveOrUpdate(buildTsxJsonToSave);
+    console.log(buildTsxJsonToSave);
 
-   const buildScssJsonToSave = {
-     filename: inputValue + '.scss',
-     content: parseJsonToScss(modules[0])   
- }  
-  saveOrUpdate(buildScssJsonToSave);
+    const buildScssJsonToSave = {
+      filename: inputValue + '.scss',
+      content: parseJsonToScss(modules[0]),
+    };
+    saveOrUpdate(buildScssJsonToSave);
   };
 
   //Recoge el objeto que se va a guardar y comprueba si existe en el back para decidir si hace un post o update
   const saveOrUpdate = (build) => {
-    const {filename, content} = build;
+    const { filename, content } = build;
     getFiles(path)
-    .then((data: any) => {
-      const fileExists = data.find((obj: any) => obj.name === filename);
-      if (fileExists) {
-        console.log('El archivo existe');
-        dispatch(updateFile(build));
-      } else {
-        console.log('El archivo no existe');
-        dispatch(postFile(build));
-        return false;
-      }
-    })
-    .catch((error: any) => {
-      console.log(error);
-    });
-  }
-  
-//Devuelve un Json con array de objetos con información de los módulos
+      .then((data: any) => {
+        const fileExists = data.find((obj: any) => obj.name === filename);
+        if (fileExists) {
+          console.log('El archivo existe');
+          dispatch(updateFile(build));
+        } else {
+          console.log('El archivo no existe');
+          dispatch(postFile(build));
+          return false;
+        }
+      })
+      .catch((error: any) => {
+        console.log(error);
+      });
+  };
+
+  //Devuelve un Json con array de objetos con información de los módulos
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -164,7 +161,6 @@ export const Editor = () => {
     };
     fetchData();
   }, []);
-  
 
   const componentDef = parse(`export const ScreenSample = () => {
         return (
@@ -344,31 +340,31 @@ export const Editor = () => {
             </ResponsiveGridLayout>
           </div>
 
-          <div className="editor-header">
-            
-          </div>
-        </Column>
-        <Column>
-        <input
+          <div className="editor-header"></div>
+        </Column>{' '}
+        <div className="columna-mia">
+          <Column>
+            <input
               onChange={(e) => setInputValue(e.target.value)}
               value={inputValue}
             ></input>
             <button onClick={handleSave}>Guardar</button>
-        </Column>
-        <div
-          className="editor__element"
-          style={{
-            marginRight: selectedElement == null ? -250 : 0,
-          }}
-        >
-          <InfoPanel
-            element={selectedElement}
-            onClose={() => setSelectedElement(null)}
-            styles={styles}
-            setStyles={setStyles}
-            text={text}
-            setText={setText}
-          />
+          </Column>
+          <div
+            className="editor__element"
+            style={{
+              marginRight: selectedElement == null ? -250 : 0,
+            }}
+          >
+            <InfoPanel
+              element={selectedElement}
+              onClose={() => setSelectedElement(null)}
+              styles={styles}
+              setStyles={setStyles}
+              text={text}
+              setText={setText}
+            />
+          </div>
         </div>
       </div>
     </DndProvider>
